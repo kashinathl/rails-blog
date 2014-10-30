f37d3ef6866898716d7dce37931ec5a76078e8a4
Kashinath <kslagdive@gmail.com>
2014-10-30 10:12:46 -0400

    initial commit.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,92 @@
+class PostsController < ApplicationController
+  before_action :authorize, except: [:index]
+
+  before_filter :shared_user, except: [:index, :show]
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # http_basic_authenticate_with name: "admin", password: "password", except: [ :show]
+
+  # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      Post.transaction do
+        if @post.save
+          create_shared_users(post_params)
+          format.html { redirect_to @post, notice: 'Post was successfully created.' }
+          format.json { render action: 'show', status: :created, location: @post }
+        else
+          format.html { render action: 'new' }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :body, :password, :users)
+    end
+
+    def create_shared_users(post_params)
+      users = post_params[:users]
+
+      users.split(',').each do |user|
+        shared_user = User.new(email: user.strip, password: post_params[:password], role: 'shared')
+        shared_user.save
+      end
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBlog::Application.config.secret_key_base = 'd75ac1122b7b64495c6361db923e121399960a2a5430fd6e8dba90ab3acaffee04f18a792116687038d65e1533685ddea1074449699d971cc109f9889a839b6e'
